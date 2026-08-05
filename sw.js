100f8cf7d3d74a2344191764f2e873ed6eb94f80
thmoreau4 <th.moreau4@gmail.com>
2026-08-05 09:49:22 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'paletom-v3';
-const ASSETS = ['./index.html', './manifest.json', './logo_appv2.png'];
+const CACHE = 'paletom-v4';
+const ASSETS = ['./index.html', './manifest.json', './logo_appv3.png'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
